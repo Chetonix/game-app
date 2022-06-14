a52e233244a2e9c13e06335a1b42fb2fe6670228
Chetan Harjani <chetonix@Chetans-MacBook-Pro.local>
2022-06-14 11:43:10 +0530

    add files to the main branch

---
+++ index.js
@@ -0,0 +1,48 @@
+
+window.onload = function () {
+    console.log("Happy Gaming!");
+    canvas = document.getElementById("gameCanvas");
+    canvasContext = canvas.getContext("2d");
+    framesPerSecond = 30;
+    setInterval( function () {
+        drawEverything();
+        moveEverything();
+    }, 1000/framesPerSecond);
+
+}
+
+var canvas;
+var canvasContext;
+var ballY = 600/2;
+var ballX = 10;
+var ballSpeedX = 10;
+var hasBounced = false;
+function moveEverything() {
+    ballX = ballX + ballSpeedX;
+    // ballSpeedX += 10;
+    if(ballX > 770) {
+        ballSpeedX = -ballSpeedX;
+        hasBounced = true;
+    }
+
+    if(ballX < 20) {
+        if(hasBounced) {
+            ballSpeedX = -ballSpeedX;
+        }
+    }
+}
+
+function drawEverything() {
+    canvasContext.fillStyle = "black";
+    canvasContext.fillRect(0,0, canvas.width, canvas.height);
+    canvasContext.fillStyle = "white";
+    canvasContext.fillRect(0, 250,10, 100)
+    // canvasContext.fillStyle = "green";
+    // canvasContext.fillRect(ballX, ballY, 20, 20);
+    canvasContext.beginPath();
+    canvasContext.arc(ballX, ballY, 10, 0, Math.PI*2, true);
+    canvasContext.fill();
+
+    canvasContext.fillStyle = "white";
+    canvasContext.fillRect(790, 250,10, 100);
+}
